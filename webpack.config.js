30a80e7b0d3bbd591be7ef075f9e4be88446169b
Daniel <danielspradah0@gmail.com>
2023-02-20 11:52:46 -0500

    Add webpack config to build single output file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
+/* eslint-disable */ 
+
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/index.js'),
+        // dependencies: [path.resolve(__dirname, 'src/createAppModule.js'), path.resolve(__dirname, 'src/utils.js'),]
+        // appCreate: path.resolve(__dirname, 'src/createAppModule.js'),
+        // utils: path.resolve(__dirname, 'src/utils.js'),
+    },
     output: {
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        // chunkFilename: 'common.js'
+  },
+  optimization: { //This allows to create just one output file when building
+    splitChunks: {
+      cacheGroups: {
+        common: {
+          test: /[\\/]node_modules[\\/]|[\\/]src[\\/](createAppModule|utils)\.js$/,
+          name: 'common',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 }
